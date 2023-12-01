b28d7efc71210e7f241937c492d5997e00a28bc9
daoxuantri <thegioithangkhung@gmail.com>
2023-12-01 09:38:38 +0700

    update review

--- controllers/carts.controller.js
+++ controllers/carts.controller.js
@@ -32,7 +32,9 @@ exports.addProductItemInCart = async (req,res,next)=>{
         if (productExist) {
             //kiem tra so luong trong gio hang
             const findProInCart = await Cart.findOne({_id: cartExist._id, "productItem.id": productID});
+            console.log("Cart find", findProInCart);
             const quantity1 = req.body.quantity + findProInCart.productItem[0].quantity;
+            
             const productToAdd1 = {
                 id: productID,
                 name: req.body.name,
--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -6,56 +6,60 @@ const { createError } = require("../middlewares/error");
 
 
 exports.createReview = async (req, res) => {
-    const ProductId = req.params.productId;
-    const { content, rating } = req.body;
-    const userId = req.user._id;
+  const ProductId = req.params.productId;
+  const { content, rating } = req.body;
+  const userId = req.user._id;
 
-  
-    const product = await Product.findOne({ _id: ProductId});
-    if (!product) return createError(404, "Không tìm thấy sản phẩm");
-  
-    const isAlreadyReview = await Review.findOne({
-      owner: userId,
-      parentProduct: ProductId,
-    });
-    if (isAlreadyReview)
-      return createError(200, "Bạn đã đánh giá sản phẩm này rồi");
-  
-    const newReview = new Review({
-      owner: userId,
-      parentProduct: ProductId,
-      content: content ,
-      rating: rating,
-    });
-  
-    //updating product review
-    product.reviews.push(newReview._id);
-    await product.save();
-  
-    //saving new review
-    await newReview.save();
 
-    let averageRating = 0;
-        const listReviews = await Promise.all(productInfo.reviews.map((review) => Review.findById(review)));
-        const totalRating = listReviews.reduce((total, review) => total + review.rating, 0);
-        const totalReview = listReviews.length;
-        if (totalReview !== 0) {
-            averageRating = (totalRating / totalReview).toFixed(2);
-        }
+  const product = await Product.findOne({ _id: ProductId});
+  if (!product) return createError(404, "Không tìm thấy sản phẩm");
 
-        const saveProduct = await Product.findByIdAndUpdate(
-            ProductId,
-            {
-                $set: {
-                    rating: averageRating,
-                    numReviews: totalReview,
-                },
-            },
-            { new: true },
-        );
-  
-    return res.status(200).json({
-      message: "Sản phẩm đã thêm thành công",
-      data: newReview,
-    });
-  };
+  const isAlreadyReview = await Review.findOne({
+    owner: userId,
+    parentProduct: ProductId,
+  });
+  if (isAlreadyReview)
+  {return res.status(200).json({
+    success: false,
+    message: "Sản phẩm bạn đã được đánh giá"
+  });}
+
+  const newReview = new Review({
+    owner: userId,
+    parentProduct: ProductId,
+    content: content ,
+    rating: rating,
+  });
+
+  //updating product review
+  product.reviews.push(newReview._id);
+  await product.save();
+
+  //saving new review
+  await newReview.save();
+  const productInfo = await Product.findById(req.params.productId);
+
+  let averageRating = 0;
+      const listReviews = await Promise.all(productInfo.reviews.map((review) => Review.findById(review)));
+      const totalRating = await listReviews.reduce((total, review) => total + review.rating, 0);
+      const totalReview = listReviews.length;
+      if (totalReview !== 0) {
+          averageRating = (totalRating / totalReview).toFixed(2);
+      }
+
+      const saveProduct = await Product.findByIdAndUpdate(
+          ProductId,
+          {
+              $set: {
+                  rating: averageRating,
+                  numReviews: totalReview,
+              },
+          },
+          { new: true },
+      );
+
+  return res.status(200).json({
+    message: "Sản phẩm đã thêm thành công",
+    data: newReview,
+  });
+};
